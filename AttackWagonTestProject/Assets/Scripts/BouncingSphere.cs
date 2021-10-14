58bb49df5d6c0ffe8da68321219fe20ad48b05f6
Hayden Roper <haydenroper@tamu.edu>
2021-10-14 14:55:27 -0500

    added bounce logic with force

--- AttackWagonTestProject/Assets/Scripts/BouncingSphere.cs
+++ AttackWagonTestProject/Assets/Scripts/BouncingSphere.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SphereCollider))]
 public class BouncingSphere : MonoBehaviour
 {
     public Controller controller;
     public GameObject groundPlane;
+    public float thrust = 10f;
     private Vector3 groundPosition;
     private float radius;
+    private Rigidbody physicsRB;
     // Start is called before the first frame update
     void Start()
     {
         radius = GetComponent<SphereCollider>().radius;
+        physicsRB = GetComponent<Rigidbody>();
         groundPosition = groundPlane.transform.position;
-        transform.position = new Vector3(-0.5f,(radius+groundPosition.y),-0.5f);
+        transform.position = new Vector3(-0.5f,(radius+groundPosition.y),2f);
         controller.PrimaryMouseButtonClickedEvent.AddListener(BounceBall);
     }
 
@@ -22,8 +27,9 @@ public class BouncingSphere : MonoBehaviour
         if(DidMouseHitMe(mousePosition))
         {
             //apply force to bounce ball
+            physicsRB.AddForce(transform.up * thrust);
             //Distort ball on impact
-            transform.position = transform.position + new Vector3(0f,2f,0f);
+            //transform.position = transform.position + new Vector3(0f,2f,0f);
         }
 
     }

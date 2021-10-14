11bea139c3acc2a3e8700c9d018da2f9b0672577
Hayden Roper <55068483+HaydenRoper6@users.noreply.github.com>
2021-10-13 22:23:18 -0500

    single simple sphere orbit complete

--- AttackWagonTestProject/Assets/Scripts/OrbitalSystem.cs
+++ AttackWagonTestProject/Assets/Scripts/OrbitalSystem.cs
@@ -6,16 +6,28 @@ public class OrbitalSystem : MonoBehaviour
 {
     public GameObject baseSphere;
     public List<GameObject> orbitingSpheres;
-    
+    public float speed = 1f;
+
+    private Vector3 baseSphereCenter;
+    [SerializeField]
+    private float angle = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        baseSphereCenter = baseSphere.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Keep angle between 0 and 2PI radians
+        angle = (angle + Time.deltaTime) % (2.0f * Mathf.PI);
+        float x = Mathf.Cos(angle);
+        float y = Mathf.Sin(angle);
+        float z = 0;
+        Vector3 distanceBetweenSpheres = orbitingSpheres[0].transform.position - baseSphereCenter;
+        orbitingSpheres[0].transform.position = new Vector3(x,y,z) + distanceBetweenSpheres; 
         
     }
 }

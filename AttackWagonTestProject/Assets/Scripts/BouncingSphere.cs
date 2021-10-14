6beb632442fec932b7f7be49e46410e4cd8c2555
Hayden Roper <haydenroper@tamu.edu>
2021-10-14 14:05:00 -0500

    added bounce material and logic for click detection

--- AttackWagonTestProject/Assets/Scripts/BouncingSphere.cs
+++ AttackWagonTestProject/Assets/Scripts/BouncingSphere.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class BouncingSphere : MonoBehaviour
 {
-
+    public Controller controller;
     public GameObject groundPlane;
     private Vector3 groundPosition;
     private float radius;
     // Start is called before the first frame update
     void Start()
     {
-        radius = GetComponent<Renderer>().bounds.extents.magnitude;
+        radius = GetComponent<SphereCollider>().radius;
         groundPosition = groundPlane.transform.position;
-        transform.position = groundPosition + new Vector3(0f,radius,0f);
+        transform.position = new Vector3(-0.5f,(radius+groundPosition.y),-0.5f);
+        controller.PrimaryMouseButtonClickedEvent.AddListener(BounceBall);
     }
+
+    private void BounceBall(Vector3 mousePosition)
+    {
+        if(DidMouseHitMe(mousePosition))
+        {
+            //apply force to bounce ball
+            //Distort ball on impact
+            transform.position = transform.position + new Vector3(0f,2f,0f);
+        }
+
+    }
+
+    private bool DidMouseHitMe(Vector3 mousePosition)
+    {
+        RaycastHit hit; 
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        Physics.Raycast(ray,out hit,500.0f);
+        if(hit.transform != null)
+        {
+            return hit.transform.gameObject == gameObject;
+        }
+        return false;
+        
+        //return  
+    }
+
     void Update()
     {
         
